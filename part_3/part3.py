8495e081d1a2974d4bab9e2f33f0dc5f30551bc7
Kevin Vonk <kevin@kevosoftworks.com>
2021-01-20 16:14:15 +0100

    Add question 4

--- part_3/part3.py
+++ part_3/part3.py
@@ -21,6 +21,8 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 import re
 import numpy as np
 
+from part2_lu import SparseTridiagonal
+
 def to_latex(arr, d=2, type="bmatrix"):
 	string = str(arr.round(decimals=d))
 	string = re.sub(r"^\s*\[*\s*", "", string)
@@ -86,6 +88,71 @@ def eigen_shift_inv_power(A, b0, shift, TOL=1E-8, ret_cycles=False):
 		return lambda_, b0, iter
 	else:
 		return lambda_, b0
+
+def sparse_sipi(diag, off_diag, b0, shift, TOL=1E-8):
+	MAX_ITER = 200
+
+	mat = SparseTridiagonal()
+	mat.decompose(diag - shift, off_diag)
+
+	b0 = b0 / np.linalg.norm(b0)
+	b1 = mat.solve(b0)
+	S0 = b0.T @ b1
+
+	epsilon = 999
+	iter = 0
+
+	while epsilon >= TOL and iter <= MAX_ITER:
+		b0 = b1 / np.linalg.norm(b1)
+		b1 = mat.solve(b0)
+		S1 = b0.T @ b1
+
+		epsilon = np.abs(1/S1 - 1/S0)
+		S0 = S1
+		iter += 1
+		lambda_ = 1/S0 + shift
+
+		yield lambda_, b0
+
+def sparse_double_sipi(diag, off_diag, b0, bp0, shift, TOL=1E-8):
+	MAX_ITER = 200
+
+	mat = SparseTridiagonal()
+	mat.decompose(diag - shift, off_diag)
+
+	b0 = b0 / np.linalg.norm(b0)
+	b1 = mat.solve(b0)
+	S0 = b0.T @ b1
+	b1 = b1 / np.linalg.norm(b1)
+
+	bp0 = bp0 / np.linalg.norm(bp0)
+	bp1 = mat.solve(bp0)
+	bp1 = bp1 - b1*(b1.T @ bp1)
+	Sp0 = bp0.T @ bp1
+	bp1 = bp1 / np.linalg.norm(bp1)
+
+	epsilon = False
+	iter = 0
+
+	while not epsilon and iter <= MAX_ITER:
+		b0 = b1
+		b1 = mat.solve(b0)
+		S1 = b0.T @ b1
+		b1 = b1 / np.linalg.norm(b1)
+
+		bp0 = bp1
+		bp1 = mat.solve(bp0)
+		bp1 = bp1 - b1*(b1.T @ bp1)
+		Sp1 = bp0.T @ bp1
+		bp1 = bp1 / np.linalg.norm(bp1)
+
+		epsilon = (np.abs(1/S1 - 1/S0) < TOL) and (np.abs(1/Sp1 - 1/Sp0) < TOL)
+		S0 = S1
+		Sp0 = Sp1
+		iter += 1
+		lambda_ = [1/S0 + shift, 1/Sp0 + shift]
+
+		yield lambda_, [b0, bp0]
 
 if __name__ == "__main__":
 	print("This file can only be imported")
